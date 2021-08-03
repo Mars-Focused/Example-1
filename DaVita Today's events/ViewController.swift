be370f75308cd5d59c7877eb0660d72e414c223a
Mars <BlueLynx@live.com>
2021-08-03 17:08:45 -0400

    Initial Commit

---
+++ DaVita Today's events/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaVita Today's events
+//
+//  Created by Rave Bizz on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
